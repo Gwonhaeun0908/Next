6b27aae1112e898d79ff5738d8cfeb414ea8303b
eun <haeun0908@dsm.hs.kr>
2022-12-01 21:04:44 +0900

    ✨ :: PostForm

--- next/components/PostForm.js
+++ next/components/PostForm.js
@@ -5,11 +5,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addPost } from "../reducers/post";
 
 const PostForm = () => {
-    const { imagePaths } = useSelector((state) => state.post);
-    const [text, onChangeText] = useState('');
-    const onSubmit = useCallback(() => {
+    const { imagePaths, postAdded } = useSelector(state => state.post);
+    const [text, setText] = useState('');
+    const dispatch = useDispatch();
+    const imageInput = useRef();
 
+    const onClickImageUpload = useCallback(() => {
+        imageInput.current.click();
+    }, [imageInput.current]);
+    
+    useEffect(() => {
+        if(postAdded) {
+            setText('');
+        }
+    }, [postAdded]);
+
+    const onChangeText = useCallback((e) => {
+        setText(e.target.value);
     }, []);
+
+    const onSubmit = useCallback(() => {
+        dispatch(addPost);
+    }, []);
+
     return (
         <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmit}>
             <Input.TextArea value={text} onChange={onChangeText} maxLength={140} placeholder="어떤 신기한 일이 있었나요?" />
@@ -17,17 +35,19 @@ const PostForm = () => {
                 <input type="file" multiple hidden ref={imageInput} />
                 <Button onClick={onClickImageUpload}>이미지 업로드</Button>
                 <Button type="primary" style={{ float: 'right' }} htmlType="submit">짹짹</Button>
-
             </div>
             <div>
                 {imagePaths.map((v) => {
                     return (
                         <div key={v} style={{ display:"inline-block" }}>
-                            <img src={""}
+                            <img src={"http://localhost:3000/" + v} style={{ width: '200px'}} alt={v} />
+                            <div>
+                                <Button>제거</Button>
+                            </div>
                         </div>
                     )
                 })}
             </div>
         </Form>
-    )
-}
+    );
+};
