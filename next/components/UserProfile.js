8ff0693696dcb641d9f52894b2f289cea4f26f9f
eun <haeun0908@dsm.hs.kr>
2022-12-06 23:35:03 +0900

    ♻️ :: UserProfile.js 리팩토링

--- next/components/UserProfile.js
+++ next/components/UserProfile.js
@@ -1,28 +1,29 @@
 import React, { useCallback } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Card, Avatar, Button } from 'antd';
 
 import { logoutAction } from '../reducers/user';
 
 const UserProfile = () => {
+    const { user } = useSelector(state => state.user);
     const dispatch = useDispatch();
 
     const onLogOut = useCallback(() => {
-        dispatch(logoutAction());
+        dispatch(logoutAction);
     }, []);
 
     return (
         <Card
             actions={[
-                <div key="twit">짹짹<br/>0</div>,
-                <div key="followings">팔로잉<br/>0</div>,
-                <div key="followings">팔로워<br/>0</div>,
+                <div key="twit">짹짹<br/>{user.Posts.length}</div>,
+                <div key="followings">팔로잉<br/>{user.Followings.length}</div>,
+                <div key="followings">팔로워<br/>{user.Followers.length}</div>,
             ]}
         >
             <Card.Meta
-                avatar={<Avatar>d3</Avatar>}
-                title="djii3386464"
-            /><br/>
+                avatar={<Avatar>{user.nickname[0]}</Avatar>}
+                title={user.nickname}
+            />
             <Button onClick={onLogOut}>로그아웃</Button>
         </Card>
     );
