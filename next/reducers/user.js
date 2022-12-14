7cc9ea3fa2fed84b347fbbe107ceb4a43dfade89
eun <haeun0908@dsm.hs.kr>
2022-12-14 16:59:40 +0900

    ♻️ :: user.js in reducers

--- next/reducers/user.js
+++ next/reducers/user.js
@@ -13,36 +13,70 @@ export const initialState = {
     loginData: {},
 }
 
+export const SIGN_UP = 'SIGN_UP';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const LOG_IN = 'LOG_IN'; // 액션의 이름
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'; // 액션의 이름
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE'; // 액션의 이름
+export const LOG_OUT = 'LOG_OUT';
+
+
+export const signUpAction = (data) => {
+    return {
+        type: SIGN_UP,
+        data,
+    };
+};
+
+export const signUpSuccess = {
+    type: SIGN_UP_SUCCESS,
+};
+
 export const loginAction = (data) => {
     return {
         type: 'LOG_IN',
         data,
     }
-}
+};
  
-export const logoutAction = (data) => {
-    return {
-        type: 'LOG_OUT',
-    }
-}
+export const logoutAction = {
+    type: 'LOG_OUT',
+};
 
-const reducer = (state = initialState, action) => {
+export const signUp = (data) => {
+    return {
+        type: SIGN_UP,
+        data,
+    }
+};
+
+export default (state = initialState, action) => {
     switch (action.type) {
-        case 'LOG_IN':
+        case 'LOG_IN': {
             return {
                     ...state,
                     isLoggedIn: true,
-                    name: action.data,
+                    user: dummyUser,
+                    loginData: action.data,
                 };
-        case 'LOG_OUT':
+            }
+        case 'LOG_OUT': {
             return {
                     ...state,
                     isLoggedIn: false,
                     name: null,
                 };
-        default:
-            return state;
+            }
+        case SIGN_UP: {
+            return {
+                    ...state,
+                    signUpData: action.data,
+                };
+            }
+        default: {
+            return {
+                ...state,
+            }
+        }  
     }
 };
-
-export default reducer;
