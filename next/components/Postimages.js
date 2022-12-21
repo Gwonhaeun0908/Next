77f531e76387566131b3353058b3c0302c50984e
eun <haeun0908@dsm.hs.kr>
2022-12-21 22:29:50 +0900

    🐛:: PostImages.js 수정

--- next/components/Postimages.js
+++ next/components/Postimages.js
@@ -2,6 +2,8 @@ import React, { useCallback, useState } from "react";
 import PropTypes from 'prop-types';
 import { PlusOutlined } from "@ant-design/icons";
 
+import ImagesZoom from './ImagesZoom';
+
 const PostImages = ({ images }) => {
     const [showImagesZoom, setShowImagesZoom] = useState(false);
 
@@ -16,7 +18,7 @@ const PostImages = ({ images }) => {
     if(images.length === 1) {
         return (
             <>
-                <img src={images[0].src} onClick={onZoom} />
+                <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
                 {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
         );
@@ -25,8 +27,8 @@ const PostImages = ({ images }) => {
         return (
             <>
                 <div>
-                    <img src={images[0].src} width="50%" onClink={onZoom} />
-                    <img src={images[1].src} width="50%" onClink={onZoom} />
+                    <img role="presentation" src={images[0].src} alt={images[0].src} width="50%" onClink={onZoom} />
+                    <img role="presentation" src={images[1].src} alt={images[1].src} width="50%" onClink={onZoom} />
                 </div>
                 {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
@@ -37,6 +39,7 @@ const PostImages = ({ images }) => {
             <div>
                 <img src={images[0].src} width="50%" onClick={onZoom} />
                 <div
+                    role="presentation"
                     style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
                     onClick={onZoom}
                 >
