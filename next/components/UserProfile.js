6cb75fd31ec398628b84ef6e3bd36d88eb772153
eun <haeun0908@dsm.hs.kr>
2023-01-01 17:00:12 +0900

    ♻️ :: UserProfile.js ch4 리팩토링

--- next/components/UserProfile.js
+++ next/components/UserProfile.js
@@ -2,29 +2,31 @@ import React, { useCallback } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, Avatar, Button } from 'antd';
 
-import { logoutAction } from '../reducers/user';
+import { LOG_OUT_REQUEST } from '../reducers/user';
 
 const UserProfile = () => {
-    const { user } = useSelector(state => state.user);
+    const { me, logOutLoading } = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
     const onLogOut = useCallback(() => {
-        dispatch(logoutAction);
+        dispatch({
+            type: LOG_OUT_REQUEST,
+        });
     }, []);
 
     return (
         <Card
             actions={[
-                <div key="twit">짹짹<br/>{user.Posts.length}</div>,
-                <div key="followings">팔로잉<br/>{user.Followings.length}</div>,
-                <div key="followings">팔로워<br/>{user.Followers.length}</div>,
+                <div key="twit">짹짹<br/>{me.Posts.length}</div>,
+                <div key="followings">팔로잉<br/>{me.Followings.length}</div>,
+                <div key="followings">팔로워<br/>{me.Followers.length}</div>,
             ]}
         >
             <Card.Meta
-                avatar={<Avatar>{user.nickname[0]}</Avatar>}
-                title={user.nickname}
+                avatar={<Avatar>{me.nickname[0]}</Avatar>}
+                title={me.nickname}
             />
-            <Button onClick={onLogOut}>로그아웃</Button>
+            <Button onClick={onLogOut} loading={logOutLoading}>로그아웃</Button>
         </Card>
     );
 };
