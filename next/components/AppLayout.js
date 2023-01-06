c83af46c30d185a5590f381651b40144737ecf1f
eun <haeun0908@dsm.hs.kr>
2023-01-06 09:00:58 +0900

    ♻️ :: AppLayout.js 리팩토링

--- next/components/AppLayout.js
+++ next/components/AppLayout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
@@ -13,26 +13,25 @@ const SearchInput = styled(Input.Search)`
 `;
 
 const AppLayout = ({ children }) => {
-    const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+    const { me } = useSelector((state) => state.user);
     return (
         <div>
             <Menu mode="horizontal">
-                <Menu.Item>
+                <Menu.Item key="home">
                     <Link href="/"><a>노드버드</a></Link>
                 </Menu.Item>
-                <Menu.Item>
+                <Menu.Item key="profile">
                     <Link href="/profile"><a>프로필</a></Link>
                 </Menu.Item>
-                <Menu.Item>
+                <Menu.Item key="mail">
                     <SearchInput enterButton />
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/signup"><a>회원가입</a></Link>
                 </Menu.Item>
             </Menu>
             <Row gutter={8}>
                 <Col xs={24} md={6} >
-                    {isLoggedIn ? < UserProfile /> : <LoginForm />}
+                    {me 
+                        ? < UserProfile /> 
+                        : <LoginForm />}
                 </Col>
                 <Col xs={24} md={12} >
                     {children}
