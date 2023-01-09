369ce21ac6d7f4cdcd677e3db371e87dbf531ed4
eun <haeun0908@dsm.hs.kr>
2023-01-09 17:23:29 +0900

    ♻️ :: _app.js 리팩토링

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -5,21 +5,21 @@ import Head from 'next/head';
 
 import wrapper from '../store/configureStore';
 
-const NodeBird = ({ Component }) => {
-    return (
+const NodeBird = ({ Component }) => (
         <>
             <Head>
-                <meta charSet='utf-8'/>
                 <title>NodeBird</title>
             </Head>
             <Component/>
         </>
-    );
-};
+);
 
 NodeBird.propTypes = {
     Component: PropTypes.elementType.isRequired,
-}
+};
 
+export function reportWebVitals(metric) {
+    console.log(metric);
+}
 
 export default wrapper.withRedux(NodeBird);
