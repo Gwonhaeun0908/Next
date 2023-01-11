7a6577497f3bbebbc16513501e34c860e8d84045
eun <haeun0908@dsm.hs.kr>
2023-01-11 17:36:32 +0900

    ♻️ :: profile.js 리팩토링

--- next/pages/profile.js
+++ next/pages/profile.js
@@ -1,40 +1,39 @@
 import React, { useEffect } from "react";
 import Router from "next/router";
 import { useSelector } from "react-redux";
+import Head from 'next/head';
 
 import AppLayout from "../components/AppLayout";
 import NicknameEditForm from "../components/NicknameEditForm";
 import FollowList from "../components/FollowList";
 
 const Profile = () => {
-    const { isLoggedIn } = useSelector(state => state.user);
+    const { me } = useSelector((state) => state.user);
 
     useEffect(() => {
-        if(!isLoggedIn) {
-            Router.replace('/');
+        if(!(me && me.id)) {
+            Router.push('/');
         }
-    }, [isLoggedIn])
-
-    const followerList = [{ nickname: '제로초'}, { nickname: '바보'}, { nickname: '노드버드오피셜'}];
-    const followingList = [{ nickname: '제로초'}, { nickname: '바보'}, { nickname: '노드버드오피셜'}];
+    }, [me && me.id]);
+    if(!me) {
+        return null;
+    }
 
     return (
-        <>
-            <App>
+        <AppLayout>
+            <Head>
                 <title>내 프로필 | NodeBird</title>
-            </App>
-            <AppLayout>
-                <NicknameEditForm />
-                <FollowList 
+            </Head>
+            <NicknameEditForm />
+            <FollowList 
                 header="팔로잉 목록" 
-                data={followingList} 
-                />
-                <FollowList 
+                data={me.Followings} 
+            />
+            <FollowList 
                 header="팔로워 목록" 
-                data={followerList} 
-                />
-            </AppLayout>
-        </>
+                data={me.Followers} 
+            />
+        </AppLayout>
     );
 };
 
